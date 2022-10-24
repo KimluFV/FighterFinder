a3d2b0cf02996b74af73438de69315919f80d2fa
Kim <kimariel_32@hotmail.com>
2022-10-24 00:17:27 -0300

    Clase luchador creada

---
+++ app/src/main/java/com/example/elmapa/Luchador.kt
@@ -0,0 +1,12 @@
+package com.example.elmapa
+
+class Luchador(
+    val nombre:String,
+    val apellido:String,
+    val edad:Int,
+    val altura:Int,
+    val telefono:Int,
+    val nombreusuario:String,
+    val contraseña:String,
+    var idLuchador: Int
+)
