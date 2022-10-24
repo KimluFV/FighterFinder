69ec55efcff1f2e9113cfed918176caf9093b9f2
Kim <kimariel_32@hotmail.com>
2022-10-24 12:23:17 -0300

    base de datos

--- app/src/main/java/com/example/elmapa/BasedeDatos.kt
+++ app/src/main/java/com/example/elmapa/BasedeDatos.kt
@@ -1,4 +1,33 @@
 package com.example.elmapa
 
-abstract class BasedeDatos {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities=[Luchador::class], version=1)
+abstract  class BaseDatos : RoomDatabase(){
+    abstract   fun Luchadores():LibroDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCIA:BaseDatos? = null
+        fun obtenerBaseDatos(contexto:Context):BaseDatos{
+            val bdInstancia = INSTANCIA
+            if (bdInstancia != null){
+                return bdInstancia
+            }
+
+            synchronized(this){
+                val instancia = Room.databaseBuilder(
+                    contexto.applicationContext,
+                    BaseDatos::class.java,
+                    "fighterApp"
+                ).build()
+                INSTANCIA = instancia
+                return instancia
+            }
+        }
+    }
+
 }
