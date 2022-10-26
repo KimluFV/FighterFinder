d1f8b326c46ad5853da09b1926dfbce3a2ac3c5c
Kimlu Fernández Vidal <kimariel_32@hotmail.com>
2022-10-26 12:27:54 -0300

    base de datos funcionando registrar

---
+++ app/src/main/java/com/example/elmapa/LuchadorAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.elmapa
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_luchadorvista.view.*
+
+
+class LuchadorAdapter(val contexto:Context, val luchador:List<Luchador>){
+
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
